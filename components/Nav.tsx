891a69c9421709cbe730f3261064212918658e5a
kentoverse <kentoverse@gmail.com>
2025-01-31 11:36:33 -0500

    deploy1

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,6 +4,7 @@ import { useLayoutEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Moon, Sun } from "lucide-react";
 import Github from "./logos/GitHub";
+import Hume from "./logos/Hume";
 import pkg from '@/package.json';
 import { ThemeSwitcher } from "./logos/ThemeSwitcher";
 
@@ -25,7 +26,7 @@ export const Nav = () => {
     <div className="px-4 py-2 flex items-center h-16 md:h-20 lg:h-24 z-50 bg-card border-b border-border">
       <div className="flex items-center gap-2">
         <ThemeSwitcher /> {/* Render the dynamic logo */}
-        <span className="text-md font-light">BOT DIGITAL WORKS</span>
+        <span className="text-md font-bold">BOT WORKS</span>
       </div>
       <div className="ml-auto flex items-center gap-1">
         <Button
@@ -33,8 +34,16 @@ export const Nav = () => {
           variant="ghost"
           className="ml-auto flex items-center gap-1.5"
         >
+          <Hume className="size-4" />
+          <span>Prototype</span>
+        </Button>
+        <Button
+          onClick={() => window.open(pkg.readmore, "_blank", "noopener noreferrer")}
+          variant="ghost"
+          className="ml-auto flex items-center gap-1.5"
+        >
           <Github className="size-4" />
-          <span>Star on GitHub</span>
+          <span>Read More</span>
         </Button>
         <Button
           onClick={toggleDark}
